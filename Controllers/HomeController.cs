a96d6f6cd4f738e42a3bfd8c17c13d1fa133638e
vozik <vozik@securcom.me>
2019-10-24 22:09:10 +0200

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aspNetWADropDown.Models;
+using aspNetWADropDown.ViewModels;
+
+namespace aspNetWADropDown.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult DropDownBox()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult DropDownBoxEF()
+        {
+            vozikEntities vozik = new vozikEntities();
+            ViewBag.Departments = new SelectList(vozik.Departments, "Id", "Label");
+
+            return View();
+        }
+        public ActionResult DropDownBoxWA(int? dept = 0)
+        {
+            ViewBag.dept = dept;
+
+            viewInventoryViewModel viewModel = new viewInventoryViewModel
+            {
+                selectedDepartment = ViewBag.dept
+            };
+
+            vozikEntities vozik = new vozikEntities();
+            ViewBag.Departments = new SelectList(
+                vozik.Departments.Select(d => new { Value = d.id,
+                    Text = d.Label
+                })
+                .AsEnumerable(), "Value", "Text","Selected");
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ ViewModels/viewInventoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aspNetWADropDown.ViewModels
+{
+    public class viewInventoryViewModel
+    {
+        public int selectedDepartment { get; set; }
+        public int selectedUnit { get; set; }
+    }
+}
